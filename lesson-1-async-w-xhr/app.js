3cd65a1f5d7c767ce80ffe7aa433cecb1e7fac9f
vksthomas <vksthomas@gmail.com>
2019-01-16 04:43:04 +0530

    ajax unsplash test

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,33 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        console.log(searchedForText);
+
+        (function ajaxx(){
+            const unsplashRequest = new XMLHttpRequest();
+        
+            unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+            unsplashRequest.onload = addImage;
+            unsplashRequest.setRequestHeader('Authorization', 'Client-ID 516df37e62129890c744aecd2fe369d60e851188a2006e493a13bd126b9b7aa7');
+            unsplashRequest.send();
+            })()
     });
+    
+    function addImage(){
+
+        let htmlcontent = '';
+        const data = JSON.parse(this.response);
+
+         console.log(data)
+        //  console.log(this.response)
+            const firstImage = data.results[0];
+
+            htmlcontent =  `<figure>
+            <img src='${firstImage.urls.regular}' >
+            <figure> `
+        
+
+        responseContainer.insertAdjacentHTML('afterbegin',htmlcontent)
+       
+    }
 })();
